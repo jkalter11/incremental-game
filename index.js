494fe7f92499a4bcad88ce199491d84bcbcd8448
Rachel Ralston <rachelralston@gmail.com>
2016-08-01 16:48:43 -0700

    trying to get heroku to work

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var server = express()
 var PUBLIC_DIR = __dirname + '/dist/public';
 
 
-server.set('port', process.env.PORT || 3000);
+server.set('port', process.env.PORT || 5000);
 
 server.use(express.static(PUBLIC_DIR));
 
